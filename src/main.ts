3b22cd11e637279295e31f06881bd8c5f66194b7
Nicolas D <nicolas_delperdange@hotmail.com>
2020-08-27 17:13:10 +0200

    refactor(main): doing some work :shrugging:

--- src/main.ts
+++ src/main.ts
@@ -22,36 +22,42 @@ export class GildedRose {
       const item = this.items[i];
 
       if (
-        item.name != "Aged Brie" &&
-        item.name != "Backstage passes to a TAFKAL80ETC concert"
+        item.name !== "Aged Brie" &&
+        item.name !== "Backstage passes to a TAFKAL80ETC concert"
       ) {
-        if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") {
+        if (item.quality > 0 && item.name !== "Sulfuras, Hand of Ragnaros") {
           item.quality--;
         }
       } else {
         if (item.quality < 50) {
           item.quality++;
-          if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.sellIn < 11 && item.quality < 50) {
+          if (
+            item.name == "Backstage passes to a TAFKAL80ETC concert" &&
+            item.quality < 50
+          ) {
+            if (item.sellIn < 11) {
               item.quality++;
             }
-            if (item.sellIn < 6 && item.quality < 50) {
+            if (item.sellIn < 6) {
               item.quality++;
             }
           }
         }
       }
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
+      if (item.name !== "Sulfuras, Hand of Ragnaros") {
         item.sellIn--;
       }
       if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") {
+        if (item.name !== "Aged Brie") {
+          if (item.name !== "Backstage passes to a TAFKAL80ETC concert") {
+            if (
+              item.quality > 0 &&
+              item.name !== "Sulfuras, Hand of Ragnaros"
+            ) {
               item.quality--;
             }
           } else {
-            item.quality = item.quality - item.quality;
+            item.quality = 0;
           }
         } else {
           if (item.quality < 50) {
